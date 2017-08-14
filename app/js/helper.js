9b892c91d1db779425a5dc9d06c6db310818c8e9
Tony Spegel <TonySpegel@googlemail.com>
2017-08-15 00:55:50 +0200

    Added function to determine if time is in between a lecture

--- app/js/helper.js
+++ app/js/helper.js
@@ -2,6 +2,7 @@ let house_number_input = '05';
 let floor_number_input = '02';
 let today              = 'Friday';
 let current_time       = '10:00';
+let offset_time        = '00:30';
 
 
 
@@ -72,20 +73,33 @@ function test_house_only(house_number, rooms) {
 }
 
 
-function check_time(begin, end, current) {
-    [begin_hours, begin_minutes] = begin.split(':');
-    begin_hours *= 60;
-    begin_minutes_combined = begin_hours + begin_minutes;
-    console.log(begin_minutes_combined);
+function check_time(time_begin, time_end, time_current) {
+    [begin_hours, begin_minutes]     = time_begin.split(':');
+    [end_hours, end_minutes]         = time_end.split(':');
+    [current_hours, current_minutes] = time_current.split(':');
+    begin_minutes_combined           = begin_hours * 60 + begin_minutes;
+    end_minutes_combined             = end_hours * 60 + end_minutes;
+    current_minutes_combined         = current_hours * 60 + current_minutes;
+
+    // Requested time is in between a lecture
+    if (
+        current_minutes_combined >= begin_minutes_combined && 
+        current_minutes_combined < end_minutes_combined
+    ) {
+        console.log('In Between');
+        return true;
+    }
 }
 
 function list_free_rooms(all_rooms, room, current_time) {
     let room_object = all_rooms[room];
     let lectures    = room_object.days[today];
+    let now_time    = '09:15';
+    
+    console.log(`current time is: ${now_time}`);
     
     lectures.forEach(lecture => {
-        console.log(lecture.begin);
-        check_time(lecture.begin, lecture.end, 'lol');
+        check_time(lecture.begin, lecture.end, now_time);
     });
     
     return lectures;
