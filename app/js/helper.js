7807763b20912a4374056ef731ad8eaf8bdc44f1
Tony Spegel <TonySpegel@googlemail.com>
2017-08-16 03:11:37 +0200

    Improved formatting and added file-header information

--- app/js/helper.js
+++ app/js/helper.js
@@ -1,8 +1,19 @@
+/**
+ * © 2017-08 Tony Spegel
+ * 
+ * Free Rooms EAH-Jena
+ * 
+ * Contributors:
+ * Tony Spegel
+ */
+
+// Test-presets
+// TODO: implement offset_time
 let house_number_input = '05';
-let floor_number_input = '02';
+let floor_number_input;
 let today              = 'Friday';
 let current_time       = '10:00';
-let offset_time        = '00:30';
+let offset_time        = '00:30'; // At least this much time should be available
 
 
 
@@ -10,8 +21,6 @@ window.onload = () => {
     fetch_calendar_week_json('14').then(result => {
         find_available_rooms(result);
     });
-
-    console.log(get_current_time());
 };
 
 
@@ -49,7 +58,27 @@ function set_test_state(house = undefined, floor = undefined) {
 
 
 /**
+ * Test if key matches the parameters all rooms which passes every time
+ * TODO: add return type
+ * 
+ * @param  {JSON} rooms
+ * @return {TODO}
+ */
+function test_all(rooms) {
+    // Get every room ot ouf -> 04.-1.17(SimLab)
+    let rooms_top_level_keys = Object.keys(rooms);
+    
+    // Iterate through each of this rooms
+    rooms_top_level_keys.forEach(key => {
+        console.log(`Filter all → ${key}`);
+        list_free_rooms(rooms, key);
+    });
+}
+
+
+/**
  * Test if key matches the parameters for a specific building.
+ * TODO: Add return type
  * 
  * @param  {String} house_number
  * @param  {JSON} rooms
@@ -66,48 +95,16 @@ function test_house_only(house_number, rooms) {
         
         // If house is the same as the users input
         if (house === house_number) {
-            console.log(`gefunden ${key}`);
-            console.log(list_free_rooms(rooms, key));
+            console.log(`Filter house: ${house} → ${key}`);
+            list_free_rooms(rooms, key);
         }
     });
 }
 
 
-function check_time(time_begin, time_end, time_current) {
-    [begin_hours, begin_minutes]     = time_begin.split(':');
-    [end_hours, end_minutes]         = time_end.split(':');
-    [current_hours, current_minutes] = time_current.split(':');
-    begin_minutes_combined           = begin_hours * 60 + begin_minutes;
-    end_minutes_combined             = end_hours * 60 + end_minutes;
-    current_minutes_combined         = current_hours * 60 + current_minutes;
-
-    // Requested time is in between a lecture
-    if (
-        current_minutes_combined >= begin_minutes_combined && 
-        current_minutes_combined < end_minutes_combined
-    ) {
-        console.log('In Between');
-        return true;
-    }
-}
-
-function list_free_rooms(all_rooms, room, current_time) {
-    let room_object = all_rooms[room];
-    let lectures    = room_object.days[today];
-    let now_time    = '09:15';
-    
-    console.log(`current time is: ${now_time}`);
-    
-    lectures.forEach(lecture => {
-        check_time(lecture.begin, lecture.end, now_time);
-    });
-    
-    return lectures;
-}
-
-
 /**
  * Test if key matches the parameters for a specific building and floor.
+ * TODO: Add return type
  * 
  * @param  {String} house_number
  * @param  {String} floor_number
@@ -124,26 +121,63 @@ function test_house_and_floor(house_number, floor_number, rooms) {
         
         // If both house and floor are the same as the users input
         if (house === house_number && floor === floor_number) {
-            console.log(`gefunden ${key}`);
+            console.log(`Filter house: ${house} & floor: ${floor} → ${key}`);
             list_free_rooms(rooms, key);
         }
     });
 }
 
+
 /**
- * Test if key matches the parameters all rooms which passes every time
+ * Checks if a given time is in between a lectures beginning and its end.
+ * This is achieved by converting these times in two minutes.
  * 
- * @param  {JSON} rooms
- * @return {TODO}
+ * @param  {String}     time_begin   09:30
+ * @param  {String}     time_end     11:00
+ * @param  {String}     time_current 10:15
+ * @return {Boolean}                 If a time is between begin and end
  */
-function test_all(rooms) {
-    // Get every room ot ouf -> 04.-1.17(SimLab)
-    let rooms_top_level_keys = Object.keys(rooms);
+function check_time_in_between(time_begin, time_end, time_current) {
+    [begin_hours, begin_minutes]     = time_begin.split(':');
+    [end_hours, end_minutes]         = time_end.split(':');
+    [current_hours, current_minutes] = time_current.split(':');
+    begin_minutes_combined           = begin_hours * 60 + begin_minutes;
+    end_minutes_combined             = end_hours * 60 + end_minutes;
+    current_minutes_combined         = current_hours * 60 + current_minutes;
+
+    // Requested time is in between a lecture
+    if (
+        current_minutes_combined >= begin_minutes_combined && 
+        current_minutes_combined < end_minutes_combined
+    ) {
+        console.log(
+            `${time_current} is in Between ${time_begin} and ${time_end}`
+        );
+        return true;
+    }
+}
+
+
+/**
+ * TODO: Add description and types
+ * 
+ * @param  {[type]} all_rooms    [description]
+ * @param  {[type]} room         [description]
+ * @param  {[type]} current_time [description]
+ * @return {[type]}              [description]
+ */
+function list_free_rooms(all_rooms, room, current_time) {
+    let room_object = all_rooms[room];
+    let lectures    = room_object.days[today];
+    let now_time    = '13:30';
     
-    // Iterate through each of this rooms
-    rooms_top_level_keys.forEach(key => {
-        console.log(`gefunden ${key}`);
+    // console.log(`current time is: ${now_time}`);
+    
+    lectures.forEach(lecture => {
+        check_time_in_between(lecture.begin, lecture.end, now_time);
     });
+    
+    return lectures;
 }
 
 
@@ -166,6 +200,7 @@ function find_available_rooms(rooms) {
         default:
     }
 }
+
 
 /**
  * Returns the current time
