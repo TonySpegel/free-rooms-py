f6fbdea46b63d58c9e6a3697efb1933fcf83f9d2
Tony Spegel <TonySpegel@googlemail.com>
2017-10-07 01:05:50 +0200

    Removed gulp-babel and ServiceWorker-Task

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@
 var gulp        = require('gulp');
 var browserSync = require('./node_modules/browser-sync').create();
 var sass        = require('./node_modules/gulp-sass');
-var babel       = require('./node_modules/gulp-babel');
 var reload      = browserSync.reload;
 
 // Directories
@@ -26,27 +25,6 @@ gulp.task('sass', function() {
         .pipe(sass())
         .pipe(gulp.dest(cssDirectory))
         .pipe(reload({stream: true}));
-});
-
-// Task to create a basic service-worker which caches static files
-gulp.task('sw', function(callback) {
-  var path = require('path');
-  var swPrecache = require('sw-precache');
-  var rootDir = 'app';
-
-  swPrecache.write(path.join(rootDir, 'service-worker.js'), {
-    staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,woff,woff2}'],
-    stripPrefix: rootDir
-  }, callback);
-});
-
-// Demo-Task to transpile new JS into old JS
-gulp.task('babel', function() {
-    return gulp.src('app/js/word-class.js')
-        .pipe(babel({
-            presets: ['es2015']
-        }))
-        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('default', ['serve', 'sass']);
